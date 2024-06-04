9b51264b9078b7a81e7d848e44a1e4c96ffe5f7b
rajkumar9474 <raj9434153@gmail.com>
2024-06-05 02:17:30 +0530

    new

---
+++ dummy/tic_tac_toe/logic.js
@@ -0,0 +1,68 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector(".reset_btn");
+let turnO = true;
+let newGameBtn = document.querySelector(".new_btn");
+let msgCont = document.querySelector(".msg_container")
+let msg = document.querySelector(".msg");
+let count = 0;
+
+
+const win = [[0,1,2],
+            [0,3,6],
+            [0,4,8],
+            [1,4,7],
+            [2,5,8],
+            [2,4,6],
+            [3,4,5],
+            [6,7,8]];
+
+const showWinner = (win) => {
+    msg.innerText = `Congratulation, ${(win==="O")?"first player":"second player"} is the winner`;
+    msgCont.classList.remove("hide");
+}
+
+boxes.forEach((box)=>{
+    box.addEventListener("click",()=>{
+        count++;
+    if(turnO){
+        box.innerText = "O";
+        turnO = false;
+    }
+    else{
+        box.innerText = "X";
+        turnO = true;
+    }
+    box.disabled = true;
+    console.log(count);
+    checkWinner();
+});
+});
+
+const checkWinner = ()=>{
+    for(let pat of win){
+        posVal1 = boxes[pat[0]].innerText;
+        posVal2 = boxes[pat[1]].innerText;
+        posVal3 = boxes[pat[2]].innerText;
+        if(posVal1 != "" && posVal2 != "" && posVal3 != ""){
+            if(posVal1 === posVal2 && posVal2 === posVal3){
+                boxes.forEach((box)=>{
+                    box.disabled = true;
+                })
+                showWinner(posVal1);
+            }
+        }
+    }
+};
+
+const resetGame = () => {
+    turnO =  true;
+    boxes.forEach((box)=>{
+        box.disabled = false;
+        box.innerText = "";
+    })
+    msgCont.classList.add("hide");
+    count = 0;
+}
+
+newGameBtn.addEventListener("click",resetGame);
+resetBtn.addEventListener("click",resetGame);

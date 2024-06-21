f56d61d77114b973a403e44d5646dcda77117b4b
rajkumar9474 <raj9434153@gmail.com>
2024-06-22 00:51:45 +0530

    new

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
-console.log("hello");
-let x = document.getElementById("text");
-x.addEventListener("mouseover",()=>{
-    x.style.backgroundColor = "green";
-})
+// let date = new Date();
+let date1 = Date.now()
+for(let i = 0;i<10000;i++){
+    console.log(0);
+}
+let date2 = Date.now()
+console.log(date2-date1);
